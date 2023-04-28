8ab1200e2a382309f376a4b84222d0af2d4cab43
Andrea Cancellieri <116791211+Keinssz@users.noreply.github.com>
2023-04-28 09:44:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+Console.WriteLine("Hello, World!");
+// cONNESSIONE AL DB   // pcl non pc1
+SQLiteConnection cn1 = new SQLiteConnection("chinook.db");
+var tbArtist = cn1.Query<Artist>("select* from artists");
+Console.WriteLine($"In questa tabella ci sono {tbArtist.Count}record!");
+//fine del main.... inizio della dichiarazione
+public class Artist
+{
+   public int ArtistId{get;set;}
+
+   public string? Name {get;set;}
+    
+} 
